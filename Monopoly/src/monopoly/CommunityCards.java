b26423c1249089b9b51e635b95ac2710754e1716
Arib Dhuka <atxarib99@gmail.com>
2015-11-13 14:12:49 -0600

    ConfiguredCommunit cards

--- Monopoly/src/monopoly/CommunityCards.java
+++ Monopoly/src/monopoly/CommunityCards.java
@@ -29,9 +29,35 @@ public class CommunityCards {
         "You inherit $100", 
         "From sale of stock you get $50", 
         "Holiday Fund matures - Receive $100",
-    }
+    };
+    private final int[] ids = new int[names.length];
     public CommunityCards(String name) {
         this.name = name;
+        for (int i = 1; i < ids.length + 1; i++) {
+            ids[i - 1] = i;
+        }
+    }
+    public void doAction(Player player, int id) {
+        switch(id) {
+            case 1: player.setID(0);
+            player.setCash(player.getCash() + 200); break;
+            case 2: player.setCash(player.getCash() + 75); break;
+            case 3: player.setCash(player.getCash() - 1); break;
+            case 4: player.jailCard = true;break;
+            case 5: ;break;
+            case 6: ;break;
+            case 7: ;break;
+            case 8: ;break;
+            case 9: ;break;
+            case 10: ;break;
+            case 11: ;break;
+            case 12: ;break;
+            case 13: ;break;
+            case 14: ;break;
+            case 15: ;break;
+            case 16: ;break;
+            case 17: ;break;
+            
     }
 
 }
